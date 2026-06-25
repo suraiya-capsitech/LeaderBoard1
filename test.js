590f6f1d57c2c54911fac2988d0414cbea7e35fb
suraiya-capsitech <suraiya.midda@capsitech.com>
2026-06-25 15:13:44 +0530

    ..

--- test.js
+++ test.js
@@ -1,22 +1,23 @@
 const { initializeApp, cert } = require("firebase-admin/app");
 const { getFirestore } = require("firebase-admin/firestore");
 
-const serviceAccount = require("./leaderboard-f251e-firebase-adminsdk-fbsvc-dd80814338.json");
+const serviceAccount = require("./lleaderboard-f251e-firebase-adminsdk-fbsvc-ebca134d55.json");
 
-initializeApp({
+const app = initializeApp({
   credential: cert(serviceAccount),
+  projectId: serviceAccount.project_id,
 });
 
-const db = getFirestore();
+const db = getFirestore(app);
 
-async function test() {
+console.log("Project:", serviceAccount.project_id);
+
+(async () => {
   try {
-    const snapshot = await db.collection("12112211").limit(1).get();
-    console.log("SUCCESS!");
-    console.log("Documents:", snapshot.size);
-  } catch (err) {
-    console.error(err);
+    const cols = await db.listCollections();
+    console.log("Collections:");
+    cols.forEach(c => console.log("-", c.id));
+  } catch (e) {
+    console.error(e);
   }
-}
-
-test();
+})();
